3624b492f800b9944221101dd2b94bbfaa91fef3
zwirok-git <oleksijprokopenko@gmail.com>
2026-04-21 00:34:53 +0300

    implemented test for ` restore_names`

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,61 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_name_when_first_name_is_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    result = restore_names(users)
+
+    assert result is None
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+
+def test_restore_name_when_first_name_is_missing() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_do_not_change_existing_first_name() -> None:
+    users = [
+        {
+            "first_name": "Kate",
+            "last_name": "Brown",
+            "full_name": "Katherine Brown",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Kate",
+            "last_name": "Brown",
+            "full_name": "Katherine Brown",
+        }
+    ]
